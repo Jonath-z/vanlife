fc8cdec4d3cc8a7c1eecedac272f57ed25f6f2af
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-10 20:08:07 +0200

    implement routing using useParam

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import VanList from "./pages/VanList";
+import "./server";
+import VanDetails from "./pages/VanDetails";
 
 function App() {
   return (
@@ -11,11 +14,14 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/list">list </Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/list" element={<VanList />} />
+        <Route path="/vans/:id" element={<VanDetails />} />
       </Routes>
     </BrowserRouter>
   );
